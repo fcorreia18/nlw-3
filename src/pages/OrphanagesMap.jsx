90edca60f5022773f85cad2a5c50df96e44178a7
Francisco Correia28 <francisco.correia1804@gmail.com>
2020-10-15 15:14:49 +0100

    mapa adicionado

--- src/pages/OrphanagesMap.jsx
+++ src/pages/OrphanagesMap.jsx
@@ -24,10 +24,10 @@ function OrphanagesMap() {
 
             <Map
                 center={[8.9180682, 13.2651651]}
-                zoom={16}
+                zoom={10}
                 style={{ width: '100%', height: '100%' }} >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
             </Map>
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
