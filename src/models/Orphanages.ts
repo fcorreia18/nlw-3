fff24a449172c191a463e800834ba5a19e6c41a9
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-05-17 17:23:39 +0100

    creating orphanages

--- src/models/Orphanages.ts
+++ src/models/Orphanages.ts
@@ -1,18 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('orphanages')
 export default class Orphanages {
 
+    @PrimaryGeneratedColumn('increment')
     id: number;
 
+    @Column()
     name: string;
 
+    @Column()
     latitude: number;
 
+    @Column()
     longitude: number;
 
+    @Column()
     about: string;
 
-    instructions: string;
+    @Column()
+    instrutions: string;
 
+    @Column()
     opening_hours: string;
 
+    @Column()
     open_on_weekends: boolean;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express'
+import { getRepository } from "typeorm";
+import Orphanages from './models/Orphanages'
 import './database/connection';
 
 const app: Application = express();
 app.use(express.json());
 
-app.post('/:id', (req: Request, res: Response) => {
-    res.json({ "res": "requisição feita com sucesso" });
-    console.log(req.params);
+app.post('/create-orphanage', async (req: Request, res: Response) => {
+    const { name, longitude, latitude, open_on_weekends, opening_hours, instrutions, about } = req.body;
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanage = orphanagesRepository.create({ name, longitude, latitude, open_on_weekends, opening_hours, instrutions, about });
+    await orphanagesRepository.save(orphanage);
+
     console.log(req.body);
+    res.json({ "res": "requisição feita com sucesso" });
 });
 
 app.listen(3333);
