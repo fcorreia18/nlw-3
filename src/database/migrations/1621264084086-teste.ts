c923e1afab1d15a7b74e0e458f1533fcd8d52026
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-05-17 16:15:46 +0100

    recreating the migrations and testing

---
+++ src/database/migrations/1621264084086-teste.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class teste1621264084086 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "teste",
+            columns: [
+                {
+                    name: 'testando',
+                    type: "integer",
+                    isPrimary: false,
+                    isGenerated: false,
+
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
