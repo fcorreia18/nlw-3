f859f0810ce76d36b626ead066cbba38747623fb
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-05-17 17:55:13 +0100

    starting the MVC arquitecture, abstracting the route logic in a controller

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
-
+import { getRepository } from "typeorm";
+import Orphanages from "../models/Orphanages";
 export default {
-    index(req: Request, res: Response) {
+    async index(req: Request, res: Response) {
+        const orphanagesRepository = getRepository(Orphanages);
+        const orphanages = await orphanagesRepository.find();
+        return res.status(200).json(orphanages);
+    },
 
+    async create(req: Request, res: Response) {
+        const { name, longitude, latitude, open_on_weekends, opening_hours, instrutions, about } = req.body;
+        const orphanagesRepository = getRepository(Orphanages);
+
+        const orphanage = orphanagesRepository.create({ name, longitude, latitude, open_on_weekends, opening_hours, instrutions, about });
+        await orphanagesRepository.save(orphanage);
+
+        res.status(201).json(orphanage);
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,9 @@
-import { Router, Request, Response } from "express";
-import { getRepository } from "typeorm";
-import Orphanages from './models/Orphanages'
+import { Router } from "express";
+import OrphanagesController from "./controllers/OrphanagesController";
 
 const route = Router();
 
-route.post('/create-orphanage', async (req: Request, res: Response) => {
-    const { name, longitude, latitude, open_on_weekends, opening_hours, instrutions, about } = req.body;
-    const orphanagesRepository = getRepository(Orphanages);
-
-    const orphanage = orphanagesRepository.create({ name, longitude, latitude, open_on_weekends, opening_hours, instrutions, about });
-    await orphanagesRepository.save(orphanage);
-
-    res.status(200).json(orphanage);
-});
+route.get('/list-orphanages', OrphanagesController.index);
+route.post('/create-orphanage', OrphanagesController.create);
 
 export default route;
