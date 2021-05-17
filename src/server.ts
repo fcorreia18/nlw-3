a8e3a3d0457977acf125de3728fb7c7277279e0f
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-05-17 15:39:22 +0100

    creating automated scripts to create new migrations

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
 import './database/connection';
 
-const app = express();
+const app: Application = express();
 app.use(express.json());
 
-app.post('/:id', (req, res) => {
+app.post('/:id', (req: Request, res: Response) => {
     res.json({ "res": "requisição feita com sucesso" });
     console.log(req.params);
     console.log(req.body);
