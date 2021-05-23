c390e7d536428ffa0af5d3129e28a2be0df64089
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-05-23 07:12:12 +0100

    using static paths from express to display the images

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express, { Application } from 'express'
+import express, { Application, static } from 'express'
+import path from 'path'
 import 'express-async-errors'
 import './database/connection';
 import route from './routes';
 const app: Application = express();
 app.use(express.json());
 app.use(route);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333);
