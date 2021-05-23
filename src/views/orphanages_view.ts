78d31601df4513b1863a52b065e674ee22b956fc
Francisco Correia28 <francisco.correia1804@gmail.com>
2021-05-23 07:06:16 +0100

    Creating views to set what data would be displayed to front end

--- src/views/orphanages_view.ts
+++ src/views/orphanages_view.ts
@@ -1,4 +1,5 @@
 import Orphanage from '../models/Orphanage';
+import images_view from './images_view';
 
 export default {
     render(orphanage: Orphanage) {
@@ -9,7 +10,8 @@ export default {
             open_on_weekends: orphanage.open_on_weekends,
             about: orphanage.about,
             instrutions: orphanage.instrutions,
-            opening_hours: orphanage.opening_hours
+            opening_hours: orphanage.opening_hours,
+            image: images_view.renderMany(orphanage.images)
         }
     },
     renderMany(orphanages: Orphanage[]) {
